5136bba8a08021e7af615286ebee9606f5c3d0b7
Ganesh Gaikwad <gg139086@gmail.com>
2025-09-22 23:04:53 +0530

    Simple Addition Code

---
+++ Addi.java
@@ -0,0 +1,11 @@
+// import java.util.*;
+
+public class Addi{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("The sum is: " + sum);
+    }
+}
